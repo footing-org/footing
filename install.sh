4d6a907596f78be66c285a7c3003b409d0e0b914
Wesley Kendall <wesleykendall@gmail.com>
2022-12-29 15:37:26 -0600

    Fix install script

--- install.sh
+++ install.sh
@@ -34,7 +34,19 @@ mamba_prefix=$footing_prefix/toolkits
 mamba_installer_url=https://github.com/conda-forge/miniforge/releases/download/$mamba_ver-$mambaforge_patch/Mambaforge-$mamba_ver-$mambaforge_patch-$mamba_platform-$mamba_arch.sh
 mamba_installer_dir=$(mktemp -d)
 mamba_installer_file="$mamba_installer_dir/install.sh"
-curl -L $mamba_installer_url -o $mamba_installer_file --progress-bar
+
+if command -v curl &> /dev/null
+then
+    curl -L $mamba_installer_url -o $mamba_installer_file --progress-bar
+elif command -v python3 &> /dev/null
+then
+    printf "Downloading mamba installer..."
+    python3 -c "import urllib.request; urllib.request.urlretrieve('$mamba_installer_url', '$mamba_installer_file')"
+else
+    printf "Need curl or python3 to complete installation\\n"
+    exit 2
+fi
+
 sh $mamba_installer_file -p $mamba_prefix -b -u
 
 ###
